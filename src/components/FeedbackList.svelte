457bec1fafcb8c72d5b6b535ffe1f9cefbf2cc1d
Michael McGreal <mcgrealife@gmail.com>
2021-12-07 15:57:53 -0500

    added beautiful in and out card transitions with 2 lines of simple code - wow

--- src/components/FeedbackList.svelte
+++ src/components/FeedbackList.svelte
@@ -1,5 +1,6 @@
 <script>
 import FeedbackItem from './FeedbackItem.svelte'
+import {fade, scale,slide} from 'svelte/transition'
 
 export let feedback = [];
 
@@ -9,7 +10,10 @@ export let feedback = [];
 
 {#if feedback.length > 0}
 {#each feedback as item (item.id)}
-<FeedbackItem item={item} on:delete-feedback/>
+
+<div in:scale out:slide>
+    <FeedbackItem item={item} on:delete-feedback/>
+</div>
 <!--  this 'on:delete-feedback' will be forwarded up to app.svelete, without creating and calling another createEventDispatcher. I only need to create that in the child component that initiates the event-->
 {/each}
 {:else}
